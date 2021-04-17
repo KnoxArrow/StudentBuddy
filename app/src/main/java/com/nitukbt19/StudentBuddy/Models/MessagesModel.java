6ecf6a8b3550eefc925d602b8644fae629975881
Knox Arrow <krishanandangoswami@gmail.com>
2021-04-18 00:03:23 +0530

    MessagesModel Added

---
+++ app/src/main/java/com/nitukbt19/StudentBuddy/Models/MessagesModel.java
@@ -0,0 +1,43 @@
+package com.nitukbt19.StudentBuddy.Models;
+
+public class MessagesModel {
+    // messageId from Firebase
+    //message time used for message sequencing
+    String uId,message;
+    Long timestamp;
+
+    public MessagesModel(String uId, String message) {
+        this.uId = uId;
+        this.message = message;
+    }
+
+    public MessagesModel(String uId, String message, Long timestamp) {
+        this.uId = uId;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
